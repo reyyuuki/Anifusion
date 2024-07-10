24600c306e531d7a9689240ff758beb50eb844bf
Rey Yuuki <irfan31331@gmail.com>
2024-07-10 22:17:52 +0530

    update link

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "../css/AnimeList.css";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -17,14 +16,14 @@ const AnimeList = ({ result, name }) => {
         >
           {result.map((item, index) => (
             <SwiperSlide key={index} className="AnimeCard">
-              <Link to={`/details/${item.id}`} className="IdLink">
+              <a href={`/details/${item.id}`} className="IdLink">
                 <img src={item.image} alt="anime Image" className="AnimeImage" />
                 <div className="AnimeInfo">
                   {(item.title.english || item.title.romaji).length > 20
                     ? (item.title.english || item.title.romaji).substring(0, 16) + "..."
                     : item.title.english || item.title.romaji}
                 </div>
-              </Link>
+              </a>
             </SwiperSlide>
           ))}
         </Swiper>
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -9,12 +9,12 @@ import {
   faCircleInfo,
   faCirclePlay,
 } from "@fortawesome/free-solid-svg-icons";
-import useEmblaCarousel from 'embla-carousel-react'
+import useEmblaCarousel from "embla-carousel-react";
+import Skeleton from '@mui/material/Skeleton';
 const Slider = () => {
-  
-  
   const [result, setResult] = useState([]);
   const [emblaRef] = useEmblaCarousel();
+  const [isrunning, setIsRunning] = useState(false);
   useEffect(() => {
     const Fetch = async () => {
       const data = await NewsetApi();
@@ -28,54 +28,82 @@ const Slider = () => {
     Fetch();
   }, []);
 
-  if (!result[0]) return <div>Loading</div>;
-  
+ 
+
   return (
     <div className="embla" ref={emblaRef}>
-<div className="embla__container">
-    {result.map((item,index) => (
-      <div className="embla__slide" key={index}>
-        <div className="gradient"></div>
-        <img src={item.cover} alt="anime" className="SliderImage"/>
-        <div className="sliderInfo">
-          <div className="rightBox">
-            <h1>{item.title.english}</h1>
-            <div className="iconsBox">
-              <div className="icon">
-                <FontAwesomeIcon icon={faWineGlass} className="iconStyle" />
-                <p>{item.type}</p>
-              </div>
-              <div className="icon">
-                <FontAwesomeIcon icon={faWineGlass} className="iconStyle" />
-                <p>{item.duration}</p>
-              </div>
-              <div className="icon">
-                <FontAwesomeIcon icon={faStar} className="iconStyle" />
-                <p>{item.rating / 10}</p>
-              </div>
-              <div className="icon">
-                <FontAwesomeIcon icon={faClock} className="iconStyle" />
-                <p>{item.duration} mins</p>
+      { isrunning ? (
+        <div className="embla__container">
+          {result.map((item, index) => (
+            <div className="embla__slide" key={index}>
+              <div className="gradient"></div>
+              <img src={item.cover} alt="anime" className="SliderImage" />
+              <div className="sliderInfo">
+                <div className="rightBox">
+                  <h1>{item.title.english}</h1>
+                  <div className="iconsBox">
+                    <div className="icon">
+                      <FontAwesomeIcon
+                        icon={faWineGlass}
+                        className="iconStyle"
+                      />
+                      <p>{item.type}</p>
+                    </div>
+                    <div className="icon">
+                      <FontAwesomeIcon
+                        icon={faWineGlass}
+                        className="iconStyle"
+                      />
+                      <p>{item.duration}</p>
+                    </div>
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faStar} className="iconStyle" />
+                      <p>{item.rating / 10}</p>
+                    </div>
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faClock} className="iconStyle" />
+                      <p>{item.duration} mins</p>
+                    </div>
+                  </div>
+                  <div className="description">
+                    <p>
+                      {item.description.length > 200
+                        ? item.description
+                            .replace(/<\/?[^>]+(>|$)/g, "")
+                            .substring(0, 200) + "..."
+                        : item.description.replace(/<\/?[^>]+(>|$)/g, "")}
+                    </p>
+                  </div>
+                </div>
+                <div className="info">
+                  <div className="btn">
+                    <FontAwesomeIcon
+                      icon={faCircleInfo}
+                      className="iconStyles"
+                    />
+                    <p className="iconText">Details</p>
+                  </div>
+                  <div className="btn">
+                    <FontAwesomeIcon
+                      icon={faCirclePlay}
+                      className="iconStyles"
+                    />
+                    <p className="iconText">Watch Now</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="description">
-              <p>{item.description.length > 200 ? (item.description.replace(/<\/?[^>]+(>|$)/g, "")).substring(0,200) + "..." :item.description.replace(/<\/?[^>]+(>|$)/g, "") }</p>
-            </div>
-          </div>
-          <div className="info">
-            <div className="btn">
-              <FontAwesomeIcon icon={faCircleInfo} className="iconStyles" />
-              <p className="iconText">Details</p>
-            </div>
-            <div className="btn">
-              <FontAwesomeIcon icon={faCirclePlay} className="iconStyles" />
-              <p className="iconText">Watch Now</p>
-            </div>
-          </div>
+          ))}
         </div>
-      </div>
-       ))}
-      </div>
+      ) : (
+        <Skeleton
+          variant="rectangular"
+          sx={{ bgcolor: "#1e091a" }}
+          animation="wave"
+          height={270}
+          className="Slider-Skeleton"
+        />
+      )}
     </div>
   );
 };
